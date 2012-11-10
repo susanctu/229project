435ac112e3765dda174bc7b8c8a12d2c1dc81e44
Susan Tu <susanctu@gmail.com>
2012-11-09 16:50:23 -0800

    sorry the tree may be slightly messed up prior to this commit...don't know what I did

--- loadData.py
+++ loadData.py
@@ -8,12 +8,12 @@ class TCGAData():
     """for making matrix"""
     EXPRESSION_IDX = 2
     FILE_SUFFIX = "*gene_expression_analysis.txt"
-    GENE_IDX = 1 
+
     """for making array"""
     TUMOR = 1
     NORMAL = 0
     BARCODE_IDX =1
-    SAMPLE_TYPE_IDX=6     
+    SAMPLE_TYPE_IDX=6
 
     """TODO: check that list of genes for each person are exactly same (only checked same length for now), and normalize gene expression numbers"""
 
@@ -22,32 +22,15 @@ class TCGAData():
 
     def get_gene_exp_matrix(self):
         matrix = []
-        geneNames = []
-        firstPass = True 
         for filename in sorted(glob.glob(TCGAData.FILE_SUFFIX)):#
-#            print(filename)
             file = open(filename,'r')
-            perPersonCol = []#gene expression data for 1 person, should be in same order as in original files from Dill 
-            lineNum = -1
+            perPersonCol = []#gene expression data for 1 person, should be in same order as in original files from Dill
             for line in file:
- #               print(line)
-                if lineNum==-1:
-                    lineNum = lineNum +1
-                    continue
                 line = line.rstrip('\n')
-                lineParts = line.split('\t')   
+                lineParts = line.split('\t')
                 perPersonCol.append(lineParts[TCGAData.EXPRESSION_IDX])
-                if firstPass:
-                    geneNames.append(lineParts[TCGAData.GENE_IDX])
-                else:
-  #                  print("not first file")
-   #                 print(geneNames[lineNum])
-    #                print(lineParts[TCGAData.GENE_IDX])
-                    assert(geneNames[lineNum]==lineParts[TCGAData.GENE_IDX])
-                lineNum = lineNum + 1
             matrix.append(perPersonCol)
             file.close()
-            firstPass = False
         return matrix
 
     def get_labels(self): #as a list/array
@@ -65,10 +48,10 @@ class TCGAData():
         labelList = []
         index = 0
         for barcode in sorted(barcodeToLabels.keys()):
-            labelList.append(barcodeToLabels[barcode]) 
+            labelList.append(barcodeToLabels[barcode])
             self.barcodeToIdx[barcode]=index
             index = index + 1
-        return labelList 
+        return labelList
     
     def get_index(self,barcode):#FIXME: this only works if get_labels was called first
         return self.barcodeToIdx[barcode]
@@ -78,26 +61,25 @@ def testCode():
         gene_exp = data.get_gene_exp_matrix()
         labels = data.get_labels()
         length = len(gene_exp[0])
-        #print(length)
+        print(length)
         for person_exp in gene_exp:
             if len(person_exp)!=length:
                 print(len(person_exp))
             assert(len(person_exp)==length)
-        #print(len(labels))
-        #print(len(gene_exp))
+        print(len(labels))
+        print(len(gene_exp))
         assert(len(labels)==len(gene_exp))
-        index = data.get_index('TCGA-E2-A1BD-01A-11R-A12P-07')        
+        index = data.get_index('TCGA-E2-A1BD-01A-11R-A12P-07')
+        print(gene_exp[index])
+        print(labels[index])
+        #index = data.get_index('TCGA-E2-A15E-01A-11R-A12D-07')
         #print(gene_exp[index])
         #print(labels[index])
-        index = data.get_index('TCGA-E2-A15E-01A-11R-A12D-07')        
-        #print(gene_exp[index])
-        #print(labels[index])
-        index = data.get_index('TCGA-BH-A0DP-11A-12R-A089-07')        
-        #print(gene_exp[index])
-        #print(labels[index])
+        index = data.get_index('TCGA-BH-A0DP-11A-12R-A089-07')
+        print(gene_exp[index])
+        print(labels[index])
 
         print(labels)
-        print(len(labels))
 
 
 if __name__=="__main__":

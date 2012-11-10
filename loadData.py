3f9b3b611cf19a201afe5423d0d096b3176ccbb3
Susan Tu <susanctu@gmail.com>
2012-11-09 18:02:52 -0800

    normalize, coerce to floats (0's for nulls)

--- loadData.py
+++ loadData.py
@@ -1,6 +1,8 @@
 from string import *
 import glob
 import os
+from scikits.learn import preprocessing
+import numpy
 os.chdir("../projectFiles/")
 
 class TCGAData():
@@ -15,23 +17,44 @@ class TCGAData():
     BARCODE_IDX =1
     SAMPLE_TYPE_IDX=6
 
-    """TODO: check that list of genes for each person are exactly same (only checked same length for now), and normalize gene expression numbers"""
+    """normalize gene expression numbers"""
 
     def __init__(self):
         self.barcodeToIdx = {}
 
+    def _nonzero(self, num):
+        if num == 0: 
+            return 0
+        else:
+            return 1
     def get_gene_exp_matrix(self):
-        matrix = []
+        expMatrix = []
+        genesMissingData = [0]*17814
         for filename in sorted(glob.glob(TCGAData.FILE_SUFFIX)):#
+           # missingData = False
             file = open(filename,'r')
             perPersonCol = []#gene expression data for 1 person, should be in same order as in original files from Dill
+            lineNum = -1
             for line in file:
+                if lineNum == -1:
+                    lineNum = lineNum +1
+                    continue
                 line = line.rstrip('\n')
                 lineParts = line.split('\t')
-                perPersonCol.append(lineParts[TCGAData.EXPRESSION_IDX])
-            matrix.append(perPersonCol)
+                if (lineParts[TCGAData.EXPRESSION_IDX]=="null"):
+                    genesMissingData[lineNum] = genesMissingData[lineNum] + 1
+                    perPersonCol.append(0.0)
+            #       print(line)
+                else:
+                    perPersonCol.append(float(lineParts[TCGAData.EXPRESSION_IDX]))
+                lineNum = lineNum +1
+            expMatrix.append(perPersonCol)
             file.close()
-        return matrix
+        #print(genesMissingData)
+        #genesMissingData[0]=self._nonzero(genesMissingData[0])
+        #numGenesAlwaysPresent = reduce(lambda x, y: x+self._nonzero(y), genesMissingData)
+        #print(numGenesAlwaysPresent)
+        return preprocessing.scale(numpy.matrix(expMatrix),axis=1)
 
     def get_labels(self): #as a list/array
         barcodeToLabels = {} #dict from barcode to label FIXME:do we want to keep track of this?
@@ -61,23 +84,22 @@ def testCode():
         gene_exp = data.get_gene_exp_matrix()
         labels = data.get_labels()
         length = len(gene_exp[0])
-        print(length)
+        #print(length)
         for person_exp in gene_exp:
             if len(person_exp)!=length:
                 print(len(person_exp))
             assert(len(person_exp)==length)
-        print(len(labels))
-        print(len(gene_exp))
+        #print(len(labels))
         assert(len(labels)==len(gene_exp))
         index = data.get_index('TCGA-E2-A1BD-01A-11R-A12P-07')
         print(gene_exp[index])
-        print(labels[index])
+        #print(labels[index])
         #index = data.get_index('TCGA-E2-A15E-01A-11R-A12D-07')
         #print(gene_exp[index])
         #print(labels[index])
         index = data.get_index('TCGA-BH-A0DP-11A-12R-A089-07')
-        print(gene_exp[index])
-        print(labels[index])
+        #print(gene_exp[index])
+        #print(labels[index])
 
         print(labels)
 

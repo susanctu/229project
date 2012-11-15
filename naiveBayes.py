732d2873098907d035e698d16b79e8f590420e79
Susan Tu <susanctu@gmail.com>
2012-11-14 20:25:06 -0800

    removed extra print statements

--- naiveBayes.py
+++ naiveBayes.py
@@ -6,14 +6,15 @@ def main():
     data = TCGAData()
     gene_exp = data.get_gene_exp_matrix()
     labels = data.get_labels()
-    print(len(labels))
+    
+    #print(len(labels))
     gnb = GaussianNB()
     gnb.fit(gene_exp, labels)
     predictions = []
     for exp in gene_exp:
         predictions.append(gnb.predict(exp)[0])
-    print(predictions)
-    print(len(predictions))
+    #print(predictions)
+    #print(len(predictions))
     print "Accuracy : %f" % evaluateClassifications(predictions,labels.tolist())
 
 if __name__=="__main__":

d498f6a390a41a15aeb5c51ea924ef50951f18b5
Raf Mertens <wafmertens@gmail.com>
2012-11-18 07:57:48 +0100

    feature selection now on training data

--- testUtils.py
+++ testUtils.py
@@ -1,4 +1,8 @@
 from sklearn import cross_validation
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+import numpy
+from loadData import TCGAData
 
 """This file contains useful functions for testing/evaluating different learning algorithms"""
 
@@ -13,10 +17,10 @@ def print_genes_nonzero_coeff(data,coeffs):#data should be a TCGAData object
             nonzeroNames.append(names[i])
     return nonzeroNames
 
-def kFoldCrossValid(X,Y,learningAlgo,k=2):#learningAlgo is an object, not a function! and assumes that X and Y are already numpy.arrays 
+def kFoldCrossValid(X,Y,learningAlgo,k=2,names=None,selection='none'):#learningAlgo is an object, not a function! and assumes that X and Y are already numpy.arrays 
 
         """
-        Expects matrix with feature vectors, labels, a learning algorithm, and (optionally) k.
+        Expects matrix with feature vectors, labels, a learning algorithm, and (optionally) k and a feature selection method. Currently supporting 'chi2' and 'none'.
         The learning algorithm needs to have a "fit" method that takes matrix with feature vectors and labels
         and a predict method that takes in just one feature vector and returns a list (of length 1) with the prediction
         (The fact that a list rather than a single value is returned is just due to the fact that that's what sklearn's 
@@ -32,12 +36,24 @@ def kFoldCrossValid(X,Y,learningAlgo,k=2):#learningAlgo is an object, not a func
 	    	X_train, X_test = [X[i] for i in train_index], [X[i] for i in test_index]
 	    	y_train, y_test = [Y[i] for i in train_index], [Y[i] for i in test_index]
 		#print "Xtrain has %d examples, ytrain has %d labels" % (len(X_train),len(y_train))
+    		if(selection=='chi2'):
+			numFeatures = len(X_train[0])
+			numExamples = len(y_train)
+			fs = SelectKBest(chi2,k=20)
+			fs.fit(X_train,y_train)
+			indices =  fs.get_support() #I think this gives you a bit mask of which features you want
+			names =numpy.array(names)
+			print names[indices]
+			X_train = numpy.array(X_train)
+			X_train = X_train[:,indices]
+			X_test = numpy.array(X_test)
+			X_test = X_test[:,indices]
 		learningAlgo.fit(X_train,y_train)
                 predictions = []
                 for x_vec in X_test:
                     predictions.append(learningAlgo.predict(x_vec)[0])
                 accuracy.append(evaluateClassifications(predictions,y_test))
-        return(accuracy)
+        return accuracy
 
 def evaluateClassifications(predicted,testLabels):
         """Returns a list [accuracy,precision,recall, fp, fn, tp, tn]

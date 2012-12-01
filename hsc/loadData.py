e77c656d82973d15756d3cf1e3f94adbb57f0b8b
Susan Tu <susanctu@gmail.com>
2012-11-30 16:58:13 -0800

    ridiculously sleepy right now, but I think this is right

--- hsc/loadData.py
+++ hsc/loadData.py
@@ -5,9 +5,14 @@ import numpy
 class Data:
 
     FILE_LOC = '../../hscData/' #see dropbox folder called hscData for files
+    ARRAY_ID_LEN = 9
     def __init__(self):
         self.geneNames = []
         self.cellTypes = []
+        self.isNormal = [] #boolean, is this array normal or not?
+        self.arrayToCellType = {}
+        self._make_arrayToCellType()
+        self._make_cellTypes()
 
     def get_gene_names(self):
         if not self.geneNames:
@@ -16,24 +21,42 @@ class Data:
                 self.geneNames.append(line.rstrip('\n'))#some of these might be ---
         return(self.geneNames)        
 
+    def _make_arrayToCellType(self):
+        broadFile = open(Data.FILE_LOC + 'BroadArrayList.txt')
+        for line in broadFile:
+            lineParts = line.rstrip('\n').split('\t')
+            self.arrayToCellType[lineParts[0]]=lineParts[2]
+
+    def _make_cellTypes(self): 
+        file = open(Data.FILE_LOC + 'expression.txt','r')
+        arrayNames = file.readline().rstrip('\n').split('\t')
+        for arrayName in arrayNames:
+            shortName = arrayName[0:Data.ARRAY_ID_LEN]
+            if shortName in self.arrayToCellType:
+                  self.cellTypes.append(self.arrayToCellType[shortName])
+                  self.isNormal.append(True)
+            else:
+                self.isNormal.append(False)
+  
     def get_labels(self):
-        if not self.cellTypes:
-            file = open(Data.FILE_LOC + 'meanExp.txt','r')
-            self.cellTypes =file.readline().rstrip('\n').split(' ')
         return(self.cellTypes)
 
     def get_gene_exp_matrix(self):
-        expMatrix = [[0]*11927]*38#initialize to be 38 by 11927 to avoid growing/copying lists
-        file = open(Data.FILE_LOC + 'meanExp.txt','r')
+        expMatrix = [[0]*11927]*sum(self.isNormal)#initialize to be number of normal arrays by 11927 to avoid growing/copying lists
+        file = open(Data.FILE_LOC + 'expression.txt','r')
         lineNum = -1 
         for line in file:
             if lineNum == -1:
                 lineNum = lineNum + 1
                 continue 
             line = line.rstrip('\n')
-            lineParts = line.split(' ')
-            assert(len(lineParts)==38)#check that for this gene we have expression data for all 38 types of cells
-            for expForCell,linePt in zip(expMatrix,lineParts):
+            lineParts = line.split('\t')
+            assert(len(lineParts)==1841)#check that for this gene we have expression data for all 38 types of cells
+            normalLineParts =[]
+            for i in range(0,len(lineParts)):
+                if self.isNormal[i]:
+                    normalLineParts.append(lineParts[i])  
+            for expForCell,linePt in zip(expMatrix,normalLineParts):
                 expForCell[lineNum]=float(linePt)
             lineNum = lineNum +1
         return preprocessing.scale(numpy.array(expMatrix),axis=1) #normalize
@@ -43,8 +66,10 @@ def test():
     geneExp = data.get_gene_exp_matrix()
     labels = data.get_labels()
     geneNames = data.get_gene_names()
-    #print(geneExp)
-    #print(labels)
+    print(len(geneExp))
+    print(len(geneExp[0]))
+    print(len(labels))
+    print(labels)
 
 if __name__=="__main__":
     test() 

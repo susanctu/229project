ee8ac4161111c5355f3033aaf4fbed88d28472e0
Susan Tu <susanctu@gmail.com>
2012-12-02 17:02:18 -0800

    confusion matrix and ECOC added

--- hsc/testUtils.py
+++ hsc/testUtils.py
@@ -3,6 +3,10 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 import numpy
 from loadData import Data
+import pylab
+from numpy import *
+import matplotlib.pyplot as plt
+from pylab import *
 
 """This file contains useful functions for testing/evaluating different learning algorithms"""
 
@@ -69,6 +73,38 @@ def kFoldCrossValid(X,Y,learningAlgo,k=210,names=None,selection='none'):#learnin
                 accuracy.append(evaluateClassifications(predictions,y_test))
         return accuracy
 
+def displayConfusion(conf_arr):#taken from this stackoverflow post: http://stackoverflow.com/questions/2897826/confusion-matrix-with-number-of-classified-misclassified-instances-on-it-python
+    norm_conf = []
+    for i in conf_arr:
+        a = 0
+        tmp_arr = []
+        a = sum(i,0)
+        for j in i:
+                tmp_arr.append(float(j)/float(a))
+        norm_conf.append(tmp_arr)
+
+    plt.clf()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    res = ax.imshow(array(norm_conf), cmap=cm.jet, interpolation='nearest')
+    for i, cas in enumerate(conf_arr):
+        for j, c in enumerate(cas):
+            if c>0:
+                plt.text(j-.2, i+.2, c, fontsize=14)
+    cb = fig.colorbar(res)
+    savefig("confmat.png", format="png")
+    plt.show()
+
+def confusionMatrix(predicted,testLabels,numClasses=38):
+    mat = []
+    for i in range(0,numClasses):
+        mat.append([0]*numClasses) 
+    for i in range(0,len(predicted)):
+        row = testLabels[i]
+        col = predicted[i]
+        mat[row][col]+=1
+    return(mat)
+ 
 def evaluateClassifications(predicted,testLabels):
         """Returns a list [accuracy,precision,recall, fp, fn, tp, tn]
            Yes, it would be nicer if I made an object with these attributes.
@@ -76,10 +112,6 @@ def evaluateClassifications(predicted,testLabels):
         """
 	numWrong = 0.0
 	numRight = 0.0
-	fp = 0.0
-	fn = 0.0
-	tp = 0.0
-	tn = 0.0
 	
 	for i in range(0,len(predicted)):
                 print(predicted[i].__str__() + ' ' + testLabels[i].__str__())
@@ -87,3 +119,12 @@ def evaluateClassifications(predicted,testLabels):
 		else: numWrong+=1
 	accuracy = numRight / (numRight + numWrong)
         return([accuracy])
+
+if __name__=="__main__":
+    predicted = [21, 6, 21, 21, 8, 2, 2, 2, 2, 2, 1, 1, 1, 3, 3, 2, 2, 2, 2, 2, 2, 2,1, 2, 2, 22, 22, 22, 0, 8, 0, 0, 7, 30, 24, 24, 24, 24, 21, 21, 0, 0,21, 24, 22, 22, 18, 18, 22, 22, 22, 21, 18, 22, 18, 18, 18, 18, 12, 12, 12, 12, 12, 12, 11, 11, 11, 11, 12, 13, 11, 12, 12, 12, 12, 12, 12, 18, 18, 24, 22, 34, 32, 18, 34, 34, 34, 24, 24, 24, 24, 24, 24, 24, 18, 18, 18, 18, 18, 22, 18, 18, 18, 18, 18, 18, 18, 18, 21, 21,21, 22, 22, 21, 21, 10, 21, 21, 21, 21, 18, 18, 18, 18, 22, 18, 22,22, 22, 24, 24, 21, 24, 7, 7, 7, 7, 7, 24, 24, 24, 8, 34, 34, 36, 36,34, 34, 34, 34, 37, 34, 6, 24, 34, 6, 30, 30, 6, 21, 0, 0, 0, 21, 21,34, 34, 34, 34, 35, 35, 35, 35, 35, 35, 35, 34, 36, 36, 36, 34, 34, 36, 36, 36, 36, 36, 36, 36, 32, 32, 32, 32, 32, 32, 32, 37, 34, 34,34, 34, 34, 37, 35, 36, 34, 36, 36, 34, 32]
+    actual = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4,4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9,
+9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 15,
+15, 15, 15, 15, 16, 16, 16, 16, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 19, 19, 19, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23, 23, 23, 23, 24, 24, 24, 24, 24, 25, 25, 25, 25, 26, 26, 26, 26, 26, 27, 27, 27, 27, 27, 28, 28, 28, 28, 29, 29, 29, 29, 30, 30, 30, 30, 30, 31, 31, 31, 31, 32, 32, 32, 32, 32, 32, 32, 33, 33, 33, 33, 33, 33, 34, 34, 34, 34, 34, 34, 34, 35, 35, 35, 35, 35, 35, 35, 36, 36, 36, 36, 36, 36, 36, 37, 37, 37, 37, 37, 37, 37]
+    displayConfusion(confusionMatrix(predicted,actual))    
+    #conf_arr = [[33,2,0,0,0,0,0,0,0,1,3], [3,31,0,0,0,0,0,0,0,0,0], [0,4,41,0,0,0,0,0,0,0,1], [0,1,0,30,0,6,0,0,0,0,1], [0,0,0,0,38,10,0,0,0,0,0], [0,0,0,3,1,39,0,0,0,0,4], [0,2,2,0,4,1,31,0,0,0,2], [0,1,0,0,0,0,0,36,0,2,0], [0,0,0,0,0,0,1,5,37,5,1], [3,0,0,0,0,0,0,0,0,39,0], [0,0,0,0,0,0,0,0,0,0,38] ]
+    #displayConfusion(conf_arr) 

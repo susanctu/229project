2d6f9599f2d82cbd87402be84af4054a2a9ebb4d
Susan Tu <susanctu@gmail.com>
2012-12-08 17:06:24 -0800

    changes mostly in load data, first time I've pushed testModels in a long time

--- hsc/loadData.py
+++ hsc/loadData.py
@@ -12,16 +12,15 @@ class Loader: #FIXME: make the classes for the testing data inherit from this
     """ 
 
     FILE_LOC = '../../hscData/' #see dropbox folder called hscData for files
-    ARRAY_ID_LEN = 9
     NUM_GENES = 11927
 
-    def __init__(self,expFile,geneListFile,arrayToTypeFile,expectedNumArrays,cellTypeIdx=2):
+    def __init__(self,expFile,geneListFile,arrayToTypeFile,expectedNumArrays,cellTypeIdx=2,arrayIDLen=9):
         self.expFile = expFile
         self.geneListFile = geneListFile
         self.arrayToTypeFile = arrayToTypeFile
         self.cellTypeIdx = cellTypeIdx #which column the cell type is in in arrayToTypeFile
         self.expectedNumArrays = expectedNumArrays #we're going to use this later, in making the matrix, to check that we don't have missing vals
-        
+        self.arrayIDLen = arrayIDLen
         self.geneNames = []
         self.cellTypes = []
         self.cellNameToCellType = {}
@@ -31,8 +30,8 @@ class Loader: #FIXME: make the classes for the testing data inherit from this
         self._make_arrayToCellType()
         self._make_cellTypes()
         self._make_cellTypeToCellName()
+        
 
-  
     def getCellName(self,cellType):
         return(self.cellTypeToCellName[cellType])   
 
@@ -59,7 +58,7 @@ class Loader: #FIXME: make the classes for the testing data inherit from this
         broadFile = open(Loader.FILE_LOC + self.arrayToTypeFile)
         numTypes = 0
         for line in broadFile:
-            lineParts = line.rstrip('\n').split('\t')
+            lineParts = line.rstrip('\n').split()
             if not lineParts[self.cellTypeIdx] in self.cellNameToCellType:
                 self.cellNameToCellType[lineParts[self.cellTypeIdx]]=numTypes
                 numTypes+=1
@@ -71,7 +70,9 @@ class Loader: #FIXME: make the classes for the testing data inherit from this
         file = open(Loader.FILE_LOC + self.expFile,'r')
         arrayNames = file.readline().rstrip('\n').split()
         for arrayName in arrayNames:
-            shortName = arrayName[0:Loader.ARRAY_ID_LEN]
+            shortName = arrayName[0:self.arrayIDLen]
+        #    print(self.arrayToCellType)
+        #    print(shortName)
             if shortName in self.arrayToCellType:
                 self.cellTypes.append(self.arrayToCellType[shortName])
                 self.isNormal.append(True)
@@ -84,6 +85,7 @@ class Loader: #FIXME: make the classes for the testing data inherit from this
 
     def get_gene_exp_matrix(self):
         numNormal = sum(self.isNormal)#initialize to be number of normal arrays by 11927 to avoid growing/copying lists
+        #print(numNormal)
         expMatrix = []
         for i in range(0,numNormal):
                 expMatrix.append([0]*Loader.NUM_GENES)
@@ -97,7 +99,8 @@ class Loader: #FIXME: make the classes for the testing data inherit from this
             line = line.rstrip('\n')
             lineParts = line.split()
             assert(len(lineParts)==self.expectedNumArrays)#check that for this gene we have data from 1841 arrays
-            normalLineParts =[]
+            #print(normalLineParts)
+            normalLineParts = []
             for i in range(0,len(lineParts)):
                 if self.isNormal[i]:
                     normalLineParts.append(lineParts[i]) 
@@ -116,6 +119,17 @@ def test():
 """
     WARNING!: The three subclasses below all keep track of their own int -> cell type mappings (i.e., for RaviAML, 0 might be BM_CMP_BM_NBM07, but for Broad, 0 is BASO), so if you train with dataset A and try to classify with dataset B, you should use A's getCellNames method to get back the names of your predicted classes.
 """
+class GSE_H60(Loader):
+    def __init__(self):
+        Loader.__init__(self,"GSE5007Arrays.txt","genes.txt","GSE5007names.txt",6,cellTypeIdx=1)    
+
+class GSE_K562(Loader):
+    def __init__(self):
+        Loader.__init__(self,"GSE1922Arrays.txt","genes.txt","GSE1922names.txt",48,cellTypeIdx=1,arrayIDLen=8)   
+ 
+class AML_easy(Loader):
+    def __init__(self):
+        Loader.__init__(self,"GSE19201Arrays.txt","genes.txt","GSEeasy.txt",6,cellTypeIdx=1)
 
 class RaviAML(Loader):
     def __init__(self):
@@ -125,21 +139,24 @@ class RaviNormal(Loader):
     def __init__(self):
         Loader.__init__(self,"raviArrays.txt","genes.txt","raviNormalArrayList.txt",54,cellTypeIdx=1) 
 
+class HardClasses(Loader):#MEP, CMP, GMP  
+    def __init__(self):
+        Loader.__init__(self,"expression.txt","genes.txt","hardClasses.txt",5897,cellTypeIdx=2) 
+    
 class Data(Loader):
     def __init__(self):
-        Loader.__init__(self,"expression.txt","genes.txt","BroadArrayList.txt",5897,cellTypeIdx=2) 
+        Loader.__init__(self,"expression.txt","genes.txt","BroadArrayList.txt",5897,cellTypeIdx=2)  
 
     def _make_arrayToCellType(self):
-        self.cellNameToCellType = {'DENDa2': 7, 'NKa2': 22, 'DENDa1': 6, 'TCEL1': 27, 'BCELLa2': 2, 'BCELLa3': 3, 'TCEL3': 29, 'ERY3-5': 11, 'ERY2': 10, 'TCEL4': 30, 'BCELLa4': 4, 'ERY1': 9, 'GRAN3': 15, 'GRAN2': 14, 'GRAN1': 13, 'BCELLa1': 1, 'NKa1': 21, 'TCEL7': 32, 'CMP': 5, 'PRE_BCELL2': 25, 'PRE_BCELL3': 26, 'EOS2': 8, 'HSC2': 17, 'HSC1': 16, 'TCEL6': 31, 'BASO': 0, 'GMP': 12, 'TCEL2': 28, 'TCEL8': 33, 'MONO2': 20, 'NKa3': 23, 'MONO1': 19, 'MEGA_MEP': 18, 'NKa4': 24}
-        
+        self.cellNameToCellType = {'DENDa2': 7, 'NKa2': 23, 'DENDa1': 6, 'TCEL1': 28, 'BCELLa2': 2, 'BCELLa3': 3, 'TCEL3': 30, 'ERY3-5': 11, 'ERY2': 10, 'TCEL4': 31, 'BCELLa4': 4, 'ERY1': 9, 'GRAN3': 15, 'GRAN2': 14, 'GRAN1': 13, 'BCELLa1': 1, 'NKa1': 22, 'TCEL7': 33, 'CMP': 5, 'PRE_BCELL2': 26, 'PRE_BCELL3': 27, 'EOS2': 8, 'HSC2': 17, 'MEGA': 18, 'HSC1': 16, 'TCEL6': 32, 'BASO': 0, 'GMP': 12, 'TCEL2': 29, 'TCEL8': 34, 'MONO2': 21, 'NKa3': 24, 'MONO1': 20, 'MEP': 19, 'NKa4': 25}
         file = open(Loader.FILE_LOC + self.arrayToTypeFile)
         for line in file:
             lineParts = line.rstrip('\n').split('\t')
             cellName = lineParts[self.cellTypeIdx]
             if cellName in ['ERY3','ERY4','ERY5']:
                 cellName = 'ERY3-5'
-            elif cellName in ['MEGA1','MEGA2','MEP']:
-                cellName = 'MEGA_MEP'
+            elif cellName in ['MEGA1','MEGA2']:
+                cellName = 'MEGA'
             self.arrayToCellType[lineParts[0]]=self.cellNameToCellType[cellName]
         file.close()
 
@@ -180,6 +197,6 @@ def testRaviNormal():
     print(len(geneExp[0]))
 
 if __name__=="__main__":
-    #testBroad() 
-    #testRaviAML()
-    testRaviNormal()
+    testBroad() 
+    testRaviAML()
+    #testRaviNormal()
--- hsc/testModel.py
+++ hsc/testModel.py
@@ -1,6 +1,40 @@
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multiclass import OneVsRestClassifier,OneVsOneClassifier,OutputCodeClassifier
 from sklearn.svm import LinearSVC
-from loadData import RaviNormal,RaviAML,Data
+from loadData import RaviNormal,RaviAML,AML_easy,GSE_H60,GSE_K562,Data
+
+def classifyWithBestECOC(trainData,testData):
+
+    train_X = trainData.get_gene_exp_matrix()
+    train_y = trainData.get_labels()
+    test_X = testData.get_gene_exp_matrix()
+    test_y = testData.get_labels()
+
+    bestSvm = LinearSVC(C=125,penalty='l1',dual=False)
+    predictions = OutputCodeClassifier(bestSvm,code_size=52).fit(train_X,train_y).predict(test_X)
+    print("ecoc --------------------")
+    print("predictions:")
+    print(trainData.getCellNames(predictions))
+    print("actual:")
+    print(testData.getCellNames(test_y))
+    print("zipped:")
+    print(zip(trainData.getCellNames(predictions),testData.getCellNames(test_y)))
+
+def classifyWithBest1vs1(trainData,testData):
+
+    train_X = trainData.get_gene_exp_matrix()
+    train_y = trainData.get_labels()
+    test_X = testData.get_gene_exp_matrix()
+    test_y = testData.get_labels()
+
+    bestSvm = LinearSVC(C=125,penalty='l1',dual=False)
+    predictions = OneVsRestClassifier(bestSvm).fit(train_X,train_y).predict(test_X)
+    print("one versus one--------------------")
+    print("predictions:")
+    print(trainData.getCellNames(predictions))
+    print("actual:")
+    print(testData.getCellNames(test_y))
+    print("zipped:")
+    print(zip(trainData.getCellNames(predictions),testData.getCellNames(test_y)))
 
 def classifyWithBest1vsAll(trainData,testData):
 
@@ -21,12 +55,29 @@ def classifyWithBest1vsAll(trainData,testData):
 def classifyRaviNormal():
     b = Data()
     n = RaviNormal()
-    classifyWithBest1vsAll(b,n) 
+    classifyWithBest1vs1(b,n) 
 
 def classifyRaviAML():
     b = Data()
     aml = RaviAML()
-    classifyWithBest1vsAll(b,aml)
- 
+    classifyWithBest1vs1(b,aml)
+
+def classifyAMLEasy():
+    b = Data()
+    amlEasy = AML_easy()
+    classifyWithBestECOC(b,amlEasy) 
+
+def classifyH60():
+    b = Data()
+    h = GSE_H60()
+    classifyWithBestECOC(b,h) 
+
+def classifyK562():
+    b = Data()
+    k = GSE_K562()
+    classifyWithBestECOC(b,k) 
+
 if __name__=="__main__":
-    classifyRaviAML()
+    classifyAMLEasy()
+    classifyH60()
+    classifyK562()
